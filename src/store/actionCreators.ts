8dc5035c6464cb71735e3c473b9ac6a759578603
theerapas <tim.apinankul@gmail.com>
2023-09-09 19:57:22 +0700

    ok2

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -1,4 +1,11 @@
+import axios from "axios"
 import * as actionTypes from "./actionTypes"
+
+axios.defaults.baseURL = 'http://localhost:8000';
+
+const GET_CURRENT_USER = 'GET_CURRENT_USER';
+const GET_CURRENT_USER_SUCCESS = 'GET_CURRENT_USER_SUCCESS';
+const GET_CURRENT_USER_FAILURE = 'GET_CURRENT_USER_FAILURE';
 
 export function addTask(task: ITask) {
   const action: TaskAction = {
@@ -17,10 +24,26 @@ export function removeTask(task: ITask) {
   return simulateHttpRequest(action)
 }
 
+export function error(task: ITask) {
+  const action: TaskAction = {
+    type: actionTypes.REMOVE_TASK,
+    task,
+  }
+  return simulateHttpRequest(action)
+}
+
 export function simulateHttpRequest(action: TaskAction) {
   return (dispatch: DispatchType) => {
-    setTimeout(() => {
-      dispatch(action)
-    }, 500)
+    axios.get('/prediction').then(  
+      user => dispatch({ type: GET_CURRENT_USER_SUCCESS, user }),
+      err => dispatch({ type: GET_CURRENT_USER_FAILURE, err }));
+  }
+}
+
+export function getPredictionList(action: TaskAction) {
+  return (dispatch: DispatchType) => {
+    axios.get('/prediction').then(  
+      PredictionTaskResponse => dispatch({ type: actionTypes.UPDATE_TASK_LIST, PredictionTaskResponse }),
+      err => dispatch({ type: GET_CURRENT_USER_FAILURE, err }));
   }
 }
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,3 +1,4 @@
+import { act } from "react-dom/test-utils"
 import * as actionTypes from "./actionTypes"
 
 const initialState: TaskState = {
@@ -12,14 +13,14 @@ const reducer = (
       case actionTypes.ADD_TASK:
         const newTask: ITask = {
           id: Date.now(),
-          title: action.task.title,
-          HLA: action.task.HLA,
-          file1: action.task.file1,
-          file2: action.task.file2,
-          file3: action.task.file3,
-          file4: action.task.file4,
-          file5: action.task.file5,
-          file6: action.task.file6,
+          name: action.task.name,
+          created_at: action.task.created_at,
+          status: act,
+          hla_type: action.task.hla_type,
+          germ_line_forward_dna: action.task.germ_line_forward_dna,
+          germ_line_reverse_dna: action.task.germ_line_reverse_dna,
+          somatic_line_forward_dna: action.task.somatic_line_forward_dna,
+          somatic_line_reverse_dna: action.task.somatic_line_reverse_dna,
         }
         return {
           ...state,
@@ -33,6 +34,23 @@ const reducer = (
           ...state,
           tasks: updatedArticles,
         }
+      case actionTypes.UPDATE_TASK_LIST:
+          const updateTaskList: ITask = {
+            id: str,
+            name: str,
+            created_at: str,
+            status: str,
+            germ_line_forward_dna: str,
+            germ_line_reverse_dna: str,
+            somatic_line_forward_dna: str,
+            somatic_line_reverse_dna: str,
+            hla_type: str,
+            result: dict,
+          }
+          return {
+            ...state,
+            tasks: state.tasks.concat(updateTaskList),
+          }
     }
     return state
   }
